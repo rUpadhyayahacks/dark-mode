ed90ceb43b895d252a7f8747a79b95061495e987
R <r@Rs-MacBook-Air.local>
2020-01-15 16:49:38 -0800

    mvp

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,14 @@
+import { useLocalStorage } from "./useLocalStorage";
+import { useEffect } from "react";
+export const useDarkMode = () => {
+  const [darkMode, setDarkMode] = useLocalStorage('darkMode', false);
+  useEffect(() => {
+    //if darkMode is true, then we add the class to the body tag, if false, we remove it
+    if (!darkMode) {
+      document.querySelector("body").classList.add("dark-mode");
+    } else {
+      document.querySelector("body").classList.remove("dark-mode");
+    }
+  }, [darkMode]);
+  return [darkMode, setDarkMode];
+};
